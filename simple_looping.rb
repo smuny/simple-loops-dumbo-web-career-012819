d99621cd6c5224cb9bb414bbb40c77e0bef2ba0b
Sharon Mun <sharonymun@gmail.com>
2019-01-10 01:14:30 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -39,7 +39,7 @@ end
 def for_iterator(number_of_times)
   # code your solution here using the "for" keyword
   # remember that `for` requires a range of numbers. How can we turn our number into a range?
-  number = 0
+  count = 0
   for count in (1..number_of_times) do
   # puts number = number + number_of_times
   count += 1
